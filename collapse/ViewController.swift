de24d71d9a9a30a36c8904a22132f9f4d176220f
Mark <bar.marcantoine@gmail.com>
2023-02-07 17:05:50 +1300

    first full commit

---
+++ collapse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collapse
+//
+//  Created by Marc-Antoine BAR on 2023-02-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
